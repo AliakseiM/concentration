89a96fc56b121fdaa4869be1032e724f920ec5fa
Алексей Михайловский <alexey@aleksejs-macbook-pro.local>
2021-11-21 20:22:34 +0300

    add Card model

---
+++ Concentration/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Алексей Михайловский on 11/21/21.
+//  Copyright © 2021 Алексей Михайловский. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+}
