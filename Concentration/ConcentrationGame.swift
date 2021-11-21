f434ee9d313d4288c95b964b98b54a9f014140ac
Алексей Михайловский <alexey@aleksejs-macbook-pro.local>
2021-11-21 20:23:19 +0300

    add ConcentrationGame class

---
+++ Concentration/ConcentrationGame.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationGame.swift
+//  Concentration
+//
+//  Created by Алексей Михайловский on 11/21/21.
+//  Copyright © 2021 Алексей Михайловский. All rights reserved.
+//
+
+import Foundation
+
+class ConcentrationGame {
+    var cards = [Card]()
+    
+    func choseCard(at index: Int) {
+        
+    }
+}
